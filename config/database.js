37a35d0cb6c0f20f32d2f2c224a6cadf9336c505
Netpoc <ayenotopeter@hotmail.com>
2022-12-09 17:08:57 +0000

    cleaning

---
+++ config/database.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const { DATABASE_URL } = process.env;
+
+exports.connect = () => {
+    mongoose
+        .connect(DATABASE_URL, {
+            useNewUrlParser: true,
+        })
+        .then(() => {
+            console.log("Database connected");
+        })
+        .catch((error) => {
+            console.log("Connection to database failed");
+            console.error(error);
+            process.exit(1);
+        });
+};
