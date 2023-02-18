c1f1829a0c492528d6a7803a34c4fd05f0d10e98
Netpoc <ayenotopeter@hotmail.com>
2023-02-18 19:36:25 +0000

    Product design map progress

--- controller/ussdController.js
+++ controller/ussdController.js
@@ -5,37 +5,44 @@ exports.ussd = [(req, res) => {
 	if (text === ""){
 		console.log(phoneNumber, "Started a session... ");
 		//This is the Shop Menu Listings
-		response = `CON Welcome to Naroke
-			1. Chemicals
-			2. Seeds
-			3. Quick Loan
+		response = `CON Welcome to Nakore
+			1. Seedlings
+			2. Chemicals
+			3. Fertilizer
 			4. Call Agent
-			5. More	
+			
 		`;
 	} else if (text === "1") {
 		// Chemical option
-		response = `CON Choose chemical \n
-		1. Gramoxono
-		2. Glaphosate
-		3. More
+		response = `CON Which crop are you planting this season?
+			1. Maize
+			2. Cassava
+			3. Rice
+			4. Others
 		`;
 	} else if (text === "2") {
 		// Seeds Option
 		response = `CON Choose Seed \n
-			1. Maize Seed
-			2. Yam Seed
-			3. Cassava Stems
-			4. Rice
+			1. Glyphosate
+			2. Atrazine 
+			3. Others			
 		`;
 	} else if (text === "3") {
 		// Quick Loan
 		response = `CON Your profile is not approved for this service \n
-			1. Request Approval
-			2. Cancel
+			1. N20 P10 K10
+			2. Urea
+			3. Others
 		`;
 	} else if (text === "4") {
 		//Call Agent
 		response = `END Call our agent on +234 706 476 7691`;
+	} else if (text === "1*1") {
+		//Maize Quantity
+		response = `CON Enter the quantity of maize seedlings you require(N100 per kg)`
+	} else if ( text === "1*2") {
+		//Cassava stems quantity
+		response = `CON Enter the bundle of stems you require (600/bundle)`
 	}
     // Print the response onto the page so that our SDK can read it
     res.set("Content-Type: text/plain");
