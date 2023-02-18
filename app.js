d7e702973b83457681ba834f07ef43123ff0aeb1
Netpoc <ayenotopeter@hotmail.com>
2023-02-18 16:52:06 +0000

    cleared

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-const port = 6000;
+const port = process.env.PORT || 6000;
 
 const ussdRoute = require("./services/ussdService");
 const userRoute = require("./services/userService");
