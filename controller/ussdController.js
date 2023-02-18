295ac97601336c4b4836682d5ae3365837ecd885
Peter Ayenoto Adeshina <netpocinc@Peters-MacBook-Air.local>
2023-02-18 08:56:43 +0000

    shop now on ussd direct

--- controller/ussdController.js
+++ controller/ussdController.js
@@ -1,64 +1,42 @@
-const Model = require('../models/models');
-
 exports.ussd = [(req, res) => {
-    // Read variables sent via POST from our SDK
-    const { sessionId, phoneNumber, text } = req.body;
+	const { sessionId, phoneNumber, text } = req.body;
     let response;
 
-    if (text === "") {
-        // Input for Event Attendee Name
-        response = 'CON Please, enter your Fullname';
-    }
-    if (text !== '') {
-        let array = text.split('*');
-        if (array.length === 1) {
-            // Input for Event Tickets
-            response = 'CON  How many tickets?'
-        }
-        else if (array.length === 2) {
-            //RESERVATION Confirmation
-            if (parseInt(array[1]) > 0) {
-                response = 'CON Do you want to save?\n1.Confirm\n2. Cancel\n3. View Tickets'
-            }
-            else {
-                response = 'End Network error. Please try again.'
-            }
-        }
-        else if (array.length === 3) {
-            //Save reservation to database 
-            if (parseInt(array[2]) === 1) {
-                let data = new Model();
-                data.name = array[0];
-                data.tickets = array[1];
-                data.save(() => {
-                    response = 'CON Your reservation was saved successfully.\n1. View Tickets'
-                })
-            }
-            else if (parseInt(array[2]) === 2) {
-                response = 'END Data was not saved.'
-            }
-            else if (parseInt(array[2]) === 3) {
-                Model.find({}, (err, users) => {
-                    console.log(users)
-                    let users_data = `${
-                        users.length < 1 ?
-                        `No data found`
-                        :
-                        `${users.map((item, index) => {
-                            return `${index + 1} ${item.name}\n`
-                        }).join(".")}`
-                    }`
-                    response = `END Current users.\n${users_data}`
-                })
-            }
-            else {
-                response = 'END Invalid input.'
-            }
-
-        }
-
-    }
-    
+	if (text === ""){
+		console.log(phoneNumber, "Started a session... ");
+		//This is the Shop Menu Listings
+		response = `CON Welcome to Naroke
+			1. Chemicals
+			2. Seeds
+			3. Quick Loan
+			4. Call Agent
+			5. More	
+		`;
+	} else if (text === "1") {
+		// Chemical option
+		response = `CON Choose chemical \n
+		1. Gramoxono
+		2. Glaphosate
+		3. More
+		`;
+	} else if (text === "2") {
+		// Seeds Option
+		response = `CON Choose Seed \n
+			1. Maize Seed
+			2. Yam Seed
+			3. Cassava Stems
+			4. Rice
+		`;
+	} else if (text === "3") {
+		// Quick Loan
+		response = `CON Your profile is not approved for this service \n
+			1. Request Approval
+			2. Cancel
+		`;
+	} else if (text === "4") {
+		//Call Agent
+		response = `END Call our agent on +234 706 476 7691`;
+	}
     // Print the response onto the page so that our SDK can read it
     res.set("Content-Type: text/plain");
     //res.send(response);
